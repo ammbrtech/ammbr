dc6f71f88c43ffe13c49749d71fa6814ce2a4d55
niksmac <nikhil@lightrains.com>
2020-05-21 12:08:17 +0530

    First commit

---
+++ precache-manifest.7e049123fe2db7283bdc95c3d414c6fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a7903f35822d3c51092a34a55a9041",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7240021f7b37228733f",
+    "url": "/static/css/2.92d80790.chunk.css"
+  },
+  {
+    "revision": "3c929b772fd6b6c01dae",
+    "url": "/static/css/main.ce206df0.chunk.css"
+  },
+  {
+    "revision": "f7240021f7b37228733f",
+    "url": "/static/js/2.b750688a.chunk.js"
+  },
+  {
+    "revision": "3c929b772fd6b6c01dae",
+    "url": "/static/js/main.c300b0e7.chunk.js"
+  },
+  {
+    "revision": "759f5ccf5faadbd0afd1",
+    "url": "/static/js/runtime-main.36011152.js"
+  },
+  {
+    "revision": "2d872348a0401c2ad75bbed678d7f4e9",
+    "url": "/static/media/2.2d872348.jpg"
+  },
+  {
+    "revision": "ea8eb97b7db43a00638310f14d9030cf",
+    "url": "/static/media/3.ea8eb97b.jpg"
+  },
+  {
+    "revision": "beba42ca5d779ff58dd5a91bde1a866d",
+    "url": "/static/media/4.beba42ca.jpg"
+  },
+  {
+    "revision": "23d984d01afc29656f04b5828e62cc8b",
+    "url": "/static/media/5.23d984d0.jpg"
+  },
+  {
+    "revision": "f968bb8f58fb3f25c7feede46efb3503",
+    "url": "/static/media/Ammbr_mesh_animation_1.f968bb8f.gif"
+  },
+  {
+    "revision": "6060b290bf44fa12b59e80c6f69513e0",
+    "url": "/static/media/s1.6060b290.png"
+  },
+  {
+    "revision": "9a03334de4e1a775e2b3e71807885bce",
+    "url": "/static/media/s2.9a03334d.png"
+  }
+]);
