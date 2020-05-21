5d0b690feaadeee853d38c74e8b889359abcf8c5
niksmac <nikhil@lightrains.com>
2020-05-21 12:19:33 +0530

    Add homepage

---
+++ precache-manifest.c7e779a0f9b38778369114c33bcdceb9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecdf9d5064ca23daec797e1b4731c6a2",
+    "url": "/ammbr/index.html"
+  },
+  {
+    "revision": "f7240021f7b37228733f",
+    "url": "/ammbr/static/css/2.92d80790.chunk.css"
+  },
+  {
+    "revision": "0030c21203eb3a4a55ec",
+    "url": "/ammbr/static/css/main.ce206df0.chunk.css"
+  },
+  {
+    "revision": "f7240021f7b37228733f",
+    "url": "/ammbr/static/js/2.b750688a.chunk.js"
+  },
+  {
+    "revision": "0030c21203eb3a4a55ec",
+    "url": "/ammbr/static/js/main.b13cf557.chunk.js"
+  },
+  {
+    "revision": "4ca99d7c16db26031b7d",
+    "url": "/ammbr/static/js/runtime-main.d57aed72.js"
+  },
+  {
+    "revision": "2d872348a0401c2ad75bbed678d7f4e9",
+    "url": "/ammbr/static/media/2.2d872348.jpg"
+  },
+  {
+    "revision": "ea8eb97b7db43a00638310f14d9030cf",
+    "url": "/ammbr/static/media/3.ea8eb97b.jpg"
+  },
+  {
+    "revision": "beba42ca5d779ff58dd5a91bde1a866d",
+    "url": "/ammbr/static/media/4.beba42ca.jpg"
+  },
+  {
+    "revision": "23d984d01afc29656f04b5828e62cc8b",
+    "url": "/ammbr/static/media/5.23d984d0.jpg"
+  },
+  {
+    "revision": "f968bb8f58fb3f25c7feede46efb3503",
+    "url": "/ammbr/static/media/Ammbr_mesh_animation_1.f968bb8f.gif"
+  },
+  {
+    "revision": "6060b290bf44fa12b59e80c6f69513e0",
+    "url": "/ammbr/static/media/s1.6060b290.png"
+  },
+  {
+    "revision": "9a03334de4e1a775e2b3e71807885bce",
+    "url": "/ammbr/static/media/s2.9a03334d.png"
+  }
+]);
